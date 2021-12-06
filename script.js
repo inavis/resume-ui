adbaade79b72c7229441adb762ce0b9063e281f9
Sivani Saravanan <31069721+inavis@users.noreply.github.com>
2021-12-06 15:04:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// let range = document.createElement("input");
+// range.setAttribute("type","range");
+// range.setAttribute("min",0);
+// range.setAttribute("max",100);
+// range.setAttribute("value",60);
+
+// var percentage1 = range.value;
+// var color1 = "yellow";
+// var percentage2 = 50;
+// var color2 = "gray";
+
+// range.style.webkitAppearance ="none";
+// range.style.background = `linear-gradient(to right ,${color1} 0%, ${color1} ${percentage1}%, ${color2} ${percentage2}% , ${color2} 100%)`;
+
+// document.body.append(range);
